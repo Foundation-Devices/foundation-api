58b6668922a214d947b2c6fa672a13f05ffc3060
Darijo <darijoperka@gmail.com>
2025-03-06 10:14:04 +0100

    Add missing steps to OnboardingState enum

--- api/src/api/onboarding.rs
+++ api/src/api/onboarding.rs
@@ -1,25 +1,37 @@
+use crate::api::quantum_link::QuantumLink;
 use {
     minicbor_derive::{Decode, Encode},
     quantum_link_macros::quantum_link,
 };
-use crate::api::quantum_link::QuantumLink;
 
 #[quantum_link]
 pub enum OnboardingState {
     #[n(0)]
     SecuringDevice,
     #[n(1)]
-    CreatingWallet,
+    DeviceSecured,
     #[n(2)]
-    CreatingMagicBackup,
+    WalletCreationScreen,
     #[n(3)]
-    CreatingManualBackup,
+    CreatingWallet,
     #[n(4)]
-    CreatingKeycardBackup,
+    WalletCreated,
     #[n(5)]
-    WritingDownSeedWords,
+    MagicBackupScreen,
     #[n(6)]
-    ConnectingWallet,
+    CreatingMagicBackup,
     #[n(7)]
+    MagicBackupCreated,
+    #[n(8)]
+    CreatingManualBackup,
+    #[n(9)]
+    CreatingKeycardBackup,
+    #[n(10)]
+    WritingDownSeedWords,
+    #[n(11)]
+    ConnectingWallet,
+    #[n(12)]
+    WalletConected,
+    #[n(13)]
     Completed,
 }
