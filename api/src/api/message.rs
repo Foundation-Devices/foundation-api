eec07644ec4af41ed81e3dde195906a31f71f3c5
Matt Gleason <mjgleason@foundationdevices.com>
2025-10-29 15:44:30 -0400

    SFT-6125: changed ID to 27

--- api/src/api/message.rs
+++ api/src/api/message.rs
@@ -103,7 +103,7 @@ pub enum QuantumLinkMessage {
     BroadcastTransaction(#[n(0)] BroadcastTransaction),
     #[n(13)]
     AccountUpdate(#[n(0)] AccountUpdate),
-    #[n(25)]
+    #[n(27)]
     ApplyPassphrase(#[n(0)] ApplyPassphrase),
     #[n(14)]
     SecurityCheck(#[n(0)] SecurityCheck),
