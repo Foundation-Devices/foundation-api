b21ab30445f0cc3532bd86aef79acbc141e87e2b
Nico Burniske <nicoburniske@gmail.com>
2026-04-15 10:55:43 -0400

    add docs

--- ql-rpc/src/lib.rs
+++ ql-rpc/src/lib.rs
@@ -35,11 +35,17 @@ impl From<u32> for RouteId {
 pub struct StreamCloseCode(pub u16);
 
 impl StreamCloseCode {
+    /// operation was cancelled
     pub const CANCELLED: Self = Self(0);
+    /// local internal error
     pub const INTERNAL: Self = Self(1);
+    /// request was refused
     pub const REFUSED: Self = Self(2);
+    /// operation timed out
     pub const TIMEOUT: Self = Self(3);
+    /// configured limit was exceeded
     pub const LIMIT: Self = Self(4);
+    /// route identifier was unknown
     pub const UNKNOWN_ROUTE: Self = Self(5);
 
     pub const fn into_inner(self) -> u16 {
